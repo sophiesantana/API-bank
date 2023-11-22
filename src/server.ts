a7dd38febd779a65aab6dab738799c6fc2e34606
Sophie Santana <ssophiesantana@gmail.com>
2023-11-22 10:20:03 -0300

    refactor: insere variavel de ambiente pra porta do servidor e adiciona as rotas ao servidor

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,16 @@
 import 'reflect-metadata';
 import express from 'express';
 import InicializationDatabase from './database/inicializationDatabase';
+import routes from './routes';
+require('dotenv').config();
 
 const app = express();
 
 app.use(express.json());
 
-const PORT = 8000;
+app.use(routes);
+
+const PORT = process.env.SERVER_PORT;
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
