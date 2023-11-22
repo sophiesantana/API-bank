7a75bcc4004b699f9f5e94840fc343a8963da4a9
Sophie Santana <ssophiesantana@gmail.com>
2023-11-22 15:37:59 -0300

    feat: cria rota de login

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { CreatePeopleController } from "./controllers/CreatePeopleController";
+import { LoginController } from "./controllers/LoginController";
 
 const routes = Router();
 
 routes.post("/people", new CreatePeopleController().handle);
+routes.post("/login", new LoginController().handle);
 
 export default routes;
