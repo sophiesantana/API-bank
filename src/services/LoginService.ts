b686a76b2d22ebffca0ec8704a57ea7a6873289e
Sophie Santana <ssophiesantana@gmail.com>
2023-11-22 15:37:15 -0300

    feat: cria LoginService e retorna token jwt

---
+++ src/services/LoginService.ts
@@ -0,0 +1,40 @@
+import AppDataSource from "../database/connection";
+import { People } from "../entities/People";
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+const secret = process.env.JWT_SECRET;
+
+const jwtConfig = {
+  expiresIn: '10m',
+  algorithm: 'HS256',
+};
+
+type PeopleRequest = {
+  document: string;
+  password: string
+}
+
+function transDocString(doc: string): string {
+  const numericString = doc.replace(/\D/g, '');
+
+  return numericString;
+}
+
+export class LoginService {
+  async execute({ document, password }: PeopleRequest): Promise<People | Error> {
+    document = transDocString(document);
+
+    const loginRepository = AppDataSource.getRepository(People);
+
+    const user = await loginRepository.findOne({ where: { document } });
+    
+    if (!user || user.password !== password) {
+      return new Error("Usuário ou senha inválidos.");
+    }
+
+    const token = jwt.sign({ data: { userId: user.id } }, secret, jwtConfig);
+
+    return token;
+  }
+}
