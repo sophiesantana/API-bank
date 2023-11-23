d9b1a26839e3543e27ec295918de9c524fb73de9
Sophie Santana <ssophiesantana@gmail.com>
2023-11-23 00:04:29 -0300

    feat: cria rotas de adicionar e buscar contas

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,15 @@
 import { Router } from "express";
 import { CreatePeopleController } from "./controllers/CreatePeopleController";
 import { LoginController } from "./controllers/LoginController";
+import { CreateAccountController } from "./controllers/CreateAccountController";
+import { GetAccountsController } from "./controllers/GetAccountsController";
+const validateJWT = require("./Auth/ValidateJWT");
 
 const routes = Router();
 
 routes.post("/people", new CreatePeopleController().handle);
 routes.post("/login", new LoginController().handle);
+routes.post("/accounts", validateJWT, new CreateAccountController().handle);
+routes.get("/accounts", validateJWT, new GetAccountsController().handle);
 
 export default routes;
