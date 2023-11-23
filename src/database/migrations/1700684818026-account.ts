dcba971d8255d9d197a12029d2223cb3f730c8af
Sophie Santana <ssophiesantana@gmail.com>
2023-11-23 00:04:46 -0300

    feat: migration contas

---
+++ src/database/migrations/1700684818026-account.ts
@@ -0,0 +1,48 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm"
+
+export class Account1700684818026 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: "account",
+                columns: [
+                    {
+                        name: "id",
+                        type: "uuid",
+                        isPrimary: true
+                    },
+                    {
+                        name: "user_id",
+                        type: "uuid",
+                        
+                    },
+                    {
+                        name: "branch",
+                        type: "varchar"
+                    },
+                    {
+                        name: "account",
+                        type: "varchar",
+                        isUnique: true
+                    },
+                    {
+                        name: "created_at",
+                        type: "timestamp",
+                        default: "now()"
+                    },
+                    {
+                        name: "updated_at",
+                        type: "timestamp",
+                        default: "now()"
+                    }
+                ]
+            })
+        )
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable("account");
+    }
+
+}
