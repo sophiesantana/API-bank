9430d6d77076b0e926f7f74b15016473037d344e
Sophie Santana <ssophiesantana@gmail.com>
2023-11-23 01:50:02 -0300

    feat: cria rota pra adicionar um cartao a uma conta

--- src/routes.ts
+++ src/routes.ts
@@ -3,6 +3,7 @@ import { CreatePeopleController } from "./controllers/CreatePeopleController";
 import { LoginController } from "./controllers/LoginController";
 import { CreateAccountController } from "./controllers/CreateAccountController";
 import { GetAccountsController } from "./controllers/GetAccountsController";
+import { CreateCardController } from "./controllers/CreateCardController";
 const validateJWT = require("./Auth/ValidateJWT");
 
 const routes = Router();
@@ -11,5 +12,7 @@ routes.post("/people", new CreatePeopleController().handle);
 routes.post("/login", new LoginController().handle);
 routes.post("/accounts", validateJWT, new CreateAccountController().handle);
 routes.get("/accounts", validateJWT, new GetAccountsController().handle);
+routes.post("/accounts/:account_id/cards", validateJWT, new CreateCardController().handle);
+
 
 export default routes;
