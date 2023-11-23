f7bbf7aa257384567a6de2f9c2c0fc0c109741a0
Sophie Santana <ssophiesantana@gmail.com>
2023-11-22 23:58:52 -0300

    feat: cria migration da conta

---
+++ src/services/CreateAccountService.ts
@@ -0,0 +1,44 @@
+import AppDataSource from "../database/connection";
+import { Account } from "../entities/Account";
+
+type AccountRequest = {
+  user_id: string
+  branch: string;
+  account: string;
+}
+
+function addHyphen(accountNum: string): string {
+  const accWithHyphen = accountNum.slice(0, accountNum.length - 1) + '-' + accountNum.slice(-1);
+
+  return accWithHyphen;
+}
+
+export class CreateAccountService {
+  async execute({ user_id, branch, account }: AccountRequest): Promise<Account | Error> {
+    account = addHyphen(account);
+
+    const accountRepository = AppDataSource.getRepository(Account);
+
+    if (
+      await accountRepository.findOne({
+        where: {
+          account
+        }
+      })
+    ) {
+      return new Error("Essa conta já foi registrada.");
+    }
+
+    const createdAccount = accountRepository.create({
+      user_id,
+      branch,
+      account,
+    })
+
+    await accountRepository.save(createdAccount);
+
+    delete createdAccount.user_id;
+
+    return createdAccount;
+  }
+}
