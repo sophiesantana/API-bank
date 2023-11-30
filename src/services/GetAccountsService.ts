f4b3bbd69a9813747a03ec043fd348e8b60d48ab
Sophie Santana <ssophiesantana@gmail.com>
2023-11-30 14:26:33 -0300

    fix: ajusta retorno ao buscar contas

--- src/services/GetAccountsService.ts
+++ src/services/GetAccountsService.ts
@@ -5,8 +5,14 @@ export class GetAccountsService {
   async execute(user_id: string): Promise<Account[] | Error> {
     const accountsRepository = AppDataSource.getRepository(Account);
 
-    const getAccounts = accountsRepository.findBy({ user_id })
+    const getAccounts = accountsRepository.findBy({ user_id });
 
-    return getAccounts;
+    const formatResult = (await getAccounts).map((account) => {
+      delete account.user_id;
+      delete account.balance;
+      return account;
+    })
+
+    return formatResult;
   }
 }
